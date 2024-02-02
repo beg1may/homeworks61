5f4690cc7cb5c79a2f8c3488f4e62f065e0fdf96
Silalieva Begimay <begimayomurovna@gmail.com>
2024-02-02 16:21:36 +0600

    Внесены базовые изменения в комонент App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CountryInfo from "./Components/CountryInfo/CountryInfo";
+
+const App = () => {
+    return (
+        <div className="country">
+            <CountryInfo/>
+        </div>
+    );
+};
+
+export default App;
